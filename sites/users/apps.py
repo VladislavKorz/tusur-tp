f561aa6e50315e295fe7bc2ab5546527626857bb
Minervaa45 <102596515+Minervaa45@users.noreply.github.com>
2023-03-27 15:36:11 +0700

    add user app

---
+++ sites/users/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.contrib import admin
+
+# from .models import Profile
+
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'external_id', 'name')
